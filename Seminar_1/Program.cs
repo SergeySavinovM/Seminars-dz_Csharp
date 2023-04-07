edc92281be96219b730dff87f77799e8c18226a9
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-07 18:25:11 +0300

    Добавил папку с первым семинаром

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,63 @@
+﻿//// Тask1
+// Console.Write("Введите число ");
+
+// int userNum = Convert.ToInt32(Console.ReadLine());
+
+// int result = userNum * userNum;
+
+// Console.WriteLine($"квадрат вашего числа равен {result}");
+
+// // Task2
+// Console.Write("Введите первое число ");
+// int userNum1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число ");
+// int userNum2 = Convert.ToInt32(Console.ReadLine());
+
+// if(userNum1 == (userNum2 * userNum2))
+// {
+//     Console.WriteLine($"Число {userNum1} является квадратом числа {userNum2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {userNum1} не является квадратом числа {userNum2}");
+// }
+
+// // Task3
+
+// Console.Write("Введите число дня недели. От 1 до 7 ");
+
+// int userNum = Convert.ToInt32(Console.ReadLine());
+
+// if(userNum == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if(userNum == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if(userNum == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if(userNum == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if(userNum == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if(userNum == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if(userNum == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели число не входящее в нужный диапазон");
+// }

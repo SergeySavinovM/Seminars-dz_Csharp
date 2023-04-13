0631f2715f870d3be3be9e02b1d6a62c25e3ed4b
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-13 11:26:18 +0300

    Добавил папку с решением первой задачи

---
+++ Dz_seminar_2/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int GetRandomInt()
+{
+    int result = new Random().Next(100, 1000);
+    return result;
+}
+
+int GetModifDigit(int number)
+{
+    int digit = number / 10;  
+    digit = digit % 10; 
+    return digit;
+}
+
+int number = GetRandomInt();
+int itog = GetModifDigit(number);
+Console.WriteLine(number);
+Console.WriteLine(itog);

dcb9c9e3ab8a4286a08054dd0c6935643b82c087
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-14 22:25:55 +0300

    Сделал третье дз

---
+++ Dz_seminar_3/Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число ");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while(count <= userNum)
+{
+    int result = (count * count) * count;
+    Console.WriteLine(result);
+    count++;
+}

8500966feffedc1e6aae6ebb6715f3685268a563
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-14 21:38:20 +0300

    Создал папку с дз по 3-му семинару

---
+++ Dz_seminar_3/Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+string Comparison(int num)
+{
+    if(num > 9999 && num < 100000)
+    {
+    int firstDigit = num / 10000;
+    int num1 = num / 1000; 
+    int secondDigit = num1 % 10;
+    int num2 = num % 100;
+    int fourthDigit = num2 / 10;
+    int fifthDigit = num % 10;
+    
+    string Comparison = "Число не является палиндромом";
+    if(firstDigit == fifthDigit && secondDigit == fourthDigit)Comparison = "Число является палиндромом";
+
+        return Comparison;
+    }
+
+    return "Вы ввели не пятизначное число";
+}
+System.Console.WriteLine(Comparison(68886));

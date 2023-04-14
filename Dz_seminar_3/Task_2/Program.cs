674e1cdd83fa65e09c850310d9f21ce57dde4f80
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-14 21:56:28 +0300

    Сделал второе дз

--- Dz_seminar_3/Task_2/Program.cs
+++ Dz_seminar_3/Task_2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double GetDist(double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double distance = Math.Pow(x1 - y1, 2) + Math.Pow(x2 - y2, 2) + Math.Pow(z1 - z2, 2);
+    distance = Math.Sqrt(distance);
+    distance = Math.Round(distance, 2);
+    return distance;
+}
+
+System.Console.WriteLine("Введите первое число по оси Х");
+double x1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число по оси Х");
+double x2 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите первое число по оси Y");
+double y1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число по оси Y");
+double y2 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите первое число по оси Z");
+double z1 = double.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Введите второе число по оси Z");
+double z2 = double.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine("Расстояние между точками = "+ GetDist(x1, y1, x2, y2, z1, z2));

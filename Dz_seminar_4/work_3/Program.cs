2ba95da80408e11074e820bbafbbdcc4acf350ae
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-19 11:31:38 +0300

    сделал третью задачу

---
+++ Dz_seminar_4/work_3/Program.cs
@@ -0,0 +1,15 @@
+﻿ //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] ArrayNumber(int length)
+{
+    int [] arr = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(1, 100);
+    }
+    return arr;
+}
+int [] RandomNumber1 = ArrayNumber(8);
+string ArrayString = string.Join(",", RandomNumber1);
+System.Console.WriteLine(ArrayString);

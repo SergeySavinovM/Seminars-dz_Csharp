112de6e24b74d811c6194d40844691dc526cf63e
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-19 11:07:11 +0300

    сделал вторую задачу

---
+++ Dz_seminar_4/work_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumOfDigits(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        int num1 = num % 10;
+        result = result + num1;
+        num = num / 10;
+    }
+    return result;
+}
+System.Console.WriteLine(SumOfDigits(11234));

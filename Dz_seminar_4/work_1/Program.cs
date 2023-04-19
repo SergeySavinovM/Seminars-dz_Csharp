3c1430645da10b77a70087cbeefaf655f4625e4d
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-19 10:31:34 +0300

    Добавил папку с дз по 4 семинару и сделал 1 задачу

---
+++ Dz_seminar_4/work_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Exponentiation(int num1, int num2)
+{
+    int result = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        result = result * num1;
+
+    }
+    return result;
+}
+System.Console.WriteLine(Exponentiation(5, 5));

dbf42e592f4e677f06dbafadeef4581950d6009b
Sergey Savinov <sergeysavinovm@gmail.com>
2023-04-25 10:27:14 +0300

    Добавил папку с решенными двумя задачами по пятому семинару

---
+++ Dz_seminar_5/work_1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] GetRandomeArray(int lenght = 5, int minValue = 100, int maxValue = 1000)   
+{
+    int [] array = new int [lenght];                                            
+    for (int i = 0; i < lenght; i++)                                            
+    {
+        array[i] = new Random().Next(minValue, maxValue);                       
+    }
+    return array;                                                               
+}
+
+int ShowNumber (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        count += 1;
+    }
+    return count;
+}
+
+int [] mass = GetRandomeArray();
+System.Console.WriteLine(string.Join(" ",mass));
+
+int number = ShowNumber(mass);
+System.Console.WriteLine(number);
---
+++ Dz_seminar_5/work_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] GetRandomeArray(int lenght = 5, int minValue = 1, int maxValue = 100)   
+{
+    int [] array = new int [lenght];                                            
+    for (int i = 0; i < lenght; i++)                                            
+    {
+        array[i] = new Random().Next(minValue, maxValue);                       
+    }
+    return array;                                                               
+}
+
+int SumNumberEvenIndex (int [] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int [] mass = GetRandomeArray();
+System.Console.WriteLine(string.Join(" ",mass));
+
+int number = SumNumberEvenIndex(mass);
+System.Console.WriteLine(number);
